962b58be3029011882199012349e13095a6f1e51
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2025-01-10 13:21:07 +0500

    .

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Register() {
+  let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let [confirm, setConfirm] = useState("");
   return (
-    <div>Register</div>
-  )
+    <div>
+      <form>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          value={confirm}
+          onChange={(e) => {
+            setConfirm(e.target.value);
+          }}
+        />
+      </form>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
